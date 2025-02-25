42928836e8e3970c71330da14a8b2724cc279dd3
OnkarShelarr <shelar.onkaros@gmail.com>
2025-02-25 20:17:32 +0530

    first commit

---
+++ src/components/HomePageComponents/Footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div><footer className="footer p-10 bg-base-200 text-base-content poppinsFont">
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover md:text-lg text-base">Branding</a>
+                <a className="link link-hover md:text-lg text-base">Design</a>
+                <a className="link link-hover md:text-lg text-base">Marketing</a>
+                <a className="link link-hover md:text-lg text-base">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover md:text-lg text-base">About us</a>
+                <a className="link link-hover md:text-lg text-base">Contact</a>
+                <a className="link link-hover md:text-lg text-base">Jobs</a>
+                <a className="link link-hover md:text-lg text-base">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover md:text-lg text-base">Terms of use</a>
+                <a className="link link-hover md:text-lg text-base">Privacy policy</a>
+                <a className="link link-hover md:text-lg text-base">Cookie policy</a>
+            </div>
+        </footer>
+            <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
+                <div className="items-center grid-flow-col">
+                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+                    <p className='font-semibold text-2xl'> BUILT BY ONKAR<br /> </p>
+                </div>
+                <div className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                    <a href='https://www.linkedin.com/in/shelaronkar/' target={'_blank'}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M22.23 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.72V1.72C24 .77 23.21 0 22.23 0zM7.12 20.45H3.56V9h3.56v11.45zM5.34 7.59c-1.14 0-2.06-.92-2.06-2.06s.92-2.06 2.06-2.06 2.06.92 2.06 2.06-.92 2.06-2.06 2.06zm15.11 12.86h-3.56v-5.57c0-1.33-.03-3.05-1.86-3.05-1.86 0-2.14 1.45-2.14 2.95v5.67H9.33V9h3.41v1.56h.05c.48-.91 1.64-1.86 3.37-1.86 3.6 0 4.27 2.37 4.27 5.45v6.3z"></path></svg></a>
+                    <a href='https://github.com/OnkarShelarr' target={'_blank'}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.207 11.385.6.11.793-.26.793-.577v-2.165c-3.34.725-4.043-1.61-4.043-1.61-.546-1.385-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.085 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.997.108-.776.42-1.305.763-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.47-2.38 1.237-3.22-.123-.303-.537-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.29-1.552 3.297-1.23 3.297-1.23.657 1.653.243 2.873.12 3.176.77.84 1.236 1.91 1.236 3.22 0 4.61-2.807 5.625-5.478 5.922.432.372.823 1.102.823 2.222v3.293c0 .32.192.693.8.575C20.565 22.092 24 17.594 24 12.297c0-6.627-5.373-12-12-12z"></path></svg></a>
+                    <a href='https://os-my-portfolio.netlify.app/' target={'_blank'}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M3 3v18h18V3H3zm16 16H5V5h14v14zM8 8h8v8H8V8z"></path></svg></a>
+                    </div>
+                </div>
+            </footer></div>
+    )
+}
+
+export default Footer
